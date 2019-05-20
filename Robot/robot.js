8161efbef27dfb886a8e4f8dbeaa76ae16a25dc7
Kem Crimmins <kem.crimmins@gmail.com>
2019-05-20 14:55:34 -0400

    create VillageState class

--- Robot/robot.js
+++ Robot/robot.js
@@ -32,3 +32,22 @@ function buildGraph(edges) {
 }
 
 const roadGraph = buildGraph(roads);
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != p.address) return p;
+        return {place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
