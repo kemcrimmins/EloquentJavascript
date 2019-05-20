72185618ed44448443205558da1940cbc1984781
Kem Crimmins <kem.crimmins@gmail.com>
2019-05-20 14:36:24 -0400

    add roads

---
+++ Robot/robot.js
@@ -0,0 +1,16 @@
+const  roads = [
+  "Alice's Hosuse-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shope",
+  "Marketplace-Farm",
+  "Marketplace-Post Office",
+  "Mareketplace-Shope"
+  "Marketplace-Town Hall",
+  "Shop-Town Hall"
+];
