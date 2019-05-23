84177277cb1d86e185dde3c1db3d08ca89b4d3b0
Kem Crimmins <kem.crimmins@gmail.com>
2019-05-23 14:25:06 -0400

    add randomRobot

--- Robot/robot.js
+++ Robot/robot.js
@@ -71,3 +71,11 @@ function runRobot(state, robot, memory) {
   }
 }
 
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place])};
+}
