c5c59496a8bc03bb1c3841175be45ab2a5389a02
Kem Crimmins <kem.crimmins@gmail.com>
2019-05-23 14:12:10 -0400

    add runRobot function

--- Robot/robot.js
+++ Robot/robot.js
@@ -58,4 +58,16 @@ let first = new VillageState(
   );
 let next = first.move("Alice's House");
 
+function runRobot(state, robot, memory) {
+  for (let turn = 0;; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
 
