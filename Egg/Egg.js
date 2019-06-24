d9958d7d289f0b335a444af37ad96ca0774b09d7
Kem Crimmins <kem.crimmins@gmail.com>
2019-06-24 16:48:57 -0400

    add parseApply

--- Egg/Egg.js
+++ Egg/Egg.js
@@ -19,3 +19,24 @@ function skipSpace(string) {
   if (first == -1) return "";
   return string.slice(first);
 }
+
+function parseApply(expr, program) {
+  program = skipSpace(program);
+  if (program[0] != "(") {
+    return {expr: expr, rest: program};
+  }
+
+  program = skipSpace(program.slice(1));
+  expr = {type: "apply", operator: expr, args: []};
+  while (program[0] != ")") {
+    let arg = parseExpression(program);
+    expr.args.push(arg.expr);
+    program = skipSpace(arg.rest);
+    if (program[0] == ",") {
+      program = skipSpace(program.slice(1));
+    } else if (program[0] != ")") {
+      throw new SyntaxError("Expected ',' or ')'");
+    }
+  }
+  return parseApply(expr, program.slice(1));
+}
