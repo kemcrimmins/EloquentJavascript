6983d7c8dcc6baec4f9eee9932672dc73fcdf723
Kem Crimmins <kem.crimmins@gmail.com>
2019-06-25 10:03:02 -0400

    add environment

--- Egg/Egg.js
+++ Egg/Egg.js
@@ -145,4 +145,21 @@ specialForms.define = (args, scope) => {
   return value;
 };
 
+const topScope = Object.create(null);
+
+topScope.true = true;
+topScope.false = false;
+
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+  topScope[op] = Function("a, b", `return a ${op} b;`);
+}
+
+topScope.print = value => {
+  console.log(value);
+  return value;
+};
+
+function run(program) {
+  return evaluate(parse(program), Object.create(topScope));
+}
 
