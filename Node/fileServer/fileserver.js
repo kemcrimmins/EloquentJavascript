f16fd02fa04be0557775f65b6c7466131461cf66
Kem Crimmins <kem.crimmins@gmail.com>
2019-07-19 13:48:19 -0400

    read directory

--- Node/fileServer/fileserver.js
+++ Node/fileServer/fileserver.js
@@ -38,3 +38,25 @@ function urlPath(url) {
   return path;
 }
 
+// Read directory
+const {createReadStream} = require("fs");
+const {stat, readdir} = require("fs").promises;
+const mime = require("mime");
+
+methods.GET = async function(request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != "ENOENT") throw error;
+    else return {status: 404, body: "File not found"};
+  }
+  if (stats.isDirectory()) {
+    return {body: (await readdir(path)).join("\n")};
+  } else {
+    return {body: createReadStream(path),
+            type: mime.getType(path)};
+  }
+};
+
