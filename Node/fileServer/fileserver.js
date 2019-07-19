3275c4272d23b058a6cbe99beca19dc14a8b2bed
Kem Crimmins <kem.crimmins@gmail.com>
2019-07-19 13:33:44 -0400

    create server

---
+++ Node/fileServer/fileserver.js
@@ -0,0 +1,25 @@
+const {createServer} = require("http");
+const methods = Object.create(null);
+
+createServer((request, response) => {
+  let handler = methods[request.method] || notAllowed;
+  handler(request)
+    .catch(error => {
+      if (error.status != null) return error;
+      return {body: String(error), status: 500};
+    })
+    .then(({body, status = 200, type = "text/plain"}) => {
+       response.writeHead(status, {"Content-Type": type});
+       if (body && body.pipe) body.pipe(response);
+       else response.end(body);
+    });
+}).listen(8000);
+
+console.log("Listening on port 8000");
+
+async function notAllowed(request) {
+  return {
+    status: 405,
+    body: `Method ${request.method} not allowed.`
+  };
+}
