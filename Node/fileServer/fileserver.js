f266937acc396aaf796a70cba739808430c9bf6d
Kem Crimmins <kem.crimmins@gmail.com>
2019-07-19 13:51:12 -0400

    handle delete requests

--- Node/fileServer/fileserver.js
+++ Node/fileServer/fileserver.js
@@ -60,3 +60,20 @@ methods.GET = async function(request) {
   }
 };
 
+// Delete requests
+const {rmdir, unlink} = require("fs").promises;
+
+methods.DELETE = async function(request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != "ENOENT") throw error;
+    else return {status: 204};
+  }
+  if (stats.isDirectory()) await rmdir(path);
+  else await unlink(path);
+  return {status: 204};
+};
+
